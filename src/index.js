567c8d9f419a77352a5dc72b1262f837d2a01c29
TMaltseva <bacardeonie@gmail.com>
2023-10-03 15:38:51 +0200

    fix case when no results (Mon, Oct , 1, 2023 9:35:24 PM)

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    return matrix == null ? [] : matrix.reduce((result, item, index) => {
+        item.sort((a, b) => index%2 ? b - a : a - b)
+            .map(element => acc.push(element));
+        return result;
+    }, []);
 }
+
